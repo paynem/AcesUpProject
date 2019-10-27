eb2a8484bbec6f565d2592de2a2da976687fb110
paynem <mpayne677@gmail.com>
2019-10-27 12:06:14 -0700

    Game should set up each column now.  These changes make it so 4 cards are dealt at beginning of game.  Clicking deal causes 4 cards to be dealt if deck isn't empty.  Also, game shuffles deck at start.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -5,34 +5,55 @@ import java.util.Collections;
 import java.util.Random;
 
 /**
- * Assignment 1: Each of the blank methods below require implementation to get AcesUp to build/run
+ * Assignment 1: Each of the blank methods below require implementation to get
+ * AcesUp to build/run
  */
 public class Game {
 
     public java.util.List<Card> deck = new ArrayList<>();
-
+    // My understanding is that List is an interface in java (not a class). Setting
+    // cols up like this gives you
+    // the flexibility to change it to something like a linked list later on if you
+    // need to.
     public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
 
-
-    public Game(){
-        // initialize a new game such that each column can store cards
+    public Game() {
+        // Since we have an array list of arraylists. We need to set up the arraylist in
+        // each index
+        ArrayList<Card> a1 = new ArrayList<Card>();
+        cols.add(a1);
+        ArrayList<Card> a2 = new ArrayList<Card>();
+        cols.add(a2);
+        ArrayList<Card> a3 = new ArrayList<Card>();
+        cols.add(a3);
+        ArrayList<Card> a4 = new ArrayList<Card>();
+        cols.add(a4);
     }
 
     public void buildDeck() {
-        for(int i = 2; i < 15; i++){
-            deck.add(new Card(i,Suit.Clubs));
-            deck.add(new Card(i,Suit.Hearts));
-            deck.add(new Card(i,Suit.Diamonds));
-            deck.add(new Card(i,Suit.Spades));
+        for (int i = 2; i < 15; i++) {
+            deck.add(new Card(i, Suit.Clubs));
+            deck.add(new Card(i, Suit.Hearts));
+            deck.add(new Card(i, Suit.Diamonds));
+            deck.add(new Card(i, Suit.Spades));
         }
     }
 
     public void shuffle() {
-        // shuffles the deck so that it is random
+        // Thankfully, Collections has a shuffle method built into it. This, well,
+        // shuffles the deck
+        Collections.shuffle(deck);
     }
 
     public void dealFour() {
-        // remove the top card from the deck and add it to a column; repeat for each of the four columns
+        // As long as the deck isn't empty, we pull 4 cards from it and put one of each
+        // of them into the 4 columns.
+        if (deck.size() != 0) {
+            for (int i = 0; i < 4; i++) {
+                addCardToCol(i, deck.get(i));
+                this.deck.remove(i);
+            }
+        }
     }
 
     public void remove(int columnNumber) {
@@ -40,14 +61,14 @@ public class Game {
     }
 
     private boolean columnHasCards(int columnNumber) {
-        // check indicated column for number of cards; if no cards return false, otherwise return true
+        // check indicated column for number of cards; if no cards return false,
+        // otherwise return true
         return false;
     }
 
     private Card getTopCard(int columnNumber) {
-        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
+        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size() - 1);
     }
-
 
     public void move(int columnFrom, int columnTo) {
         // remove the top card from the columnFrom column, add it to the columnTo column
@@ -58,6 +79,6 @@ public class Game {
     }
 
     private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
+        this.cols.get(colFrom).remove(this.cols.get(colFrom).size() - 1);
     }
 }
