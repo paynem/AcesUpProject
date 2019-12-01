32206a0a2f4aaed637f350b42a4f5f70a7103ad8
paynem <mpayne677@gmail.com>
2019-12-01 10:43:26 -0800

    more minor tweaks

--- src/main/java/models/SpanDeal.java
+++ src/main/java/models/SpanDeal.java
@@ -10,14 +10,14 @@ public class SpanDeal extends GroupOfCards {
 // DealPile's constructor generates an arraylist with 52 cards
     public SpanDeal() {
         cards = new ArrayList<>();
-        for (int i = 2; i < 14; i++) {
+        for (int i = 3; i < 15; i++) {
             cards.add(new Card(i, Card.Suit.Bastos));
             cards.add(new Card(i, Card.Suit.Oros));
             cards.add(new Card(i, Card.Suit.Copas));
             cards.add(new Card(i, Card.Suit.Espadas));
         }
-        cards.add(new Card(14, Card.Suit.Jokera));
-        cards.add(new Card(14, Card.Suit.Jokerb));
+        cards.add(new Card(15, Card.Suit.Jokera));
+        cards.add(new Card(15, Card.Suit.Jokerb));
     }
     // Shuffle uses the shuffle function from Collections to shuffle the deck
     
